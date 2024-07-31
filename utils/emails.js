08590db56518ac0609e7ed95293162bb50923d38
hamza-qasim <hamzaqasim.c@gmail.com>
2024-08-01 04:25:54 +0500

    email is updated

--- utils/emails.js
+++ utils/emails.js
@@ -183,9 +183,9 @@ Email.forgotPassword = async (user) => {
 
 Email.AwsEmailPassword = async (user) => {
 	try {
-		// const data = fs.readFileSync("./templates/awsPasswordUpdateEmail.html", "utf8");
-		const filePath = path.join(__dirname, "templates", "awsPasswordUpdateEmail.html");
-		const data = fs.readFileSync(filePath, "utf8");
+		const data = fs.readFileSync("./templates/awsPasswordUpdateEmail.html", "utf8");
+		// const filePath = path.join(__dirname, "templates", "awsPasswordUpdateEmail.html");
+		// const data = fs.readFileSync(filePath, "utf8");
 		let text = data;
 
 		const forgetPasswordToken = jwt.signToken({
