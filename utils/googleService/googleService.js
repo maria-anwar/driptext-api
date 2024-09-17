6ce94a2f357271a814fc9cd25f6d9e6d2942f0fa
Apple <apple@Apples-MacBook-Pro.local>
2024-09-17 12:50:00 +0500

    changes

--- utils/googleService/googleService.js
+++ utils/googleService/googleService.js
@@ -1,9 +1,26 @@
+// require("dotenv").config();
 const { google } = require("googleapis");
 const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env.production") });
 
+// Prepare the credentials dynamically using environment variables
+const credentials = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY, // Ensure proper newlines in the private key
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_x509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_x509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 // Initialize Google Auth and Drive/Docs API instances
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.resolve(__dirname, "./googleService_credentials.json"), // Update with your service account key file path
+  credentials,
+  // keyFile: path.resolve(__dirname, "./googleService_credentials.json"), // Update with your service account key file path
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 
