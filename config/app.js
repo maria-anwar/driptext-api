1334d5e5ecad10952366c3c667f327b27cefb6b2
Apple <apple@Apples-MacBook-Pro.local>
2024-10-22 23:21:19 +0500

    changes

--- config/app.js
+++ config/app.js
@@ -22,20 +22,20 @@ class AppConfig {
     //   "https://driptext-app.vercel.app",
     //   "https://driptext-admin-panel.vercel.app",
     // ];
-    this.app.use(
-      cors({
-        origin: function (origin, callback) {
-          // allow requests with no origin (like mobile apps or curl requests)
-          if (!origin) return callback(null, true);
-          if (allowedOrigins.indexOf(origin) === -1) {
-            const msg =
-              "The CORS policy for this site does not allow access from the specified Origin.";
-            return callback(new Error(msg), false);
-          }
-          return callback(null, true);
-        },
-      })
-    );
+    // this.app.use(
+    //   cors({
+    //     origin: function (origin, callback) {
+    //       // allow requests with no origin (like mobile apps or curl requests)
+    //       if (!origin) return callback(null, true);
+    //       if (allowedOrigins.indexOf(origin) === -1) {
+    //         const msg =
+    //           "The CORS policy for this site does not allow access from the specified Origin.";
+    //         return callback(new Error(msg), false);
+    //       }
+    //       return callback(null, true);
+    //     },
+    //   })
+    // );
     this.app.use(bodyParser.json({ limit: "5mb" }));
     this.app.use("/uploads", express.static("uploads"));
 
