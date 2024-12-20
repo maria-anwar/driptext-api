a3ff57d5dda936e2303129ab1c25c45a44962806
Apple <apple@Apples-MacBook-Pro.local>
2024-12-21 04:19:12 +0500

    changes

--- config/chargebee.js
+++ config/chargebee.js
@@ -1,7 +1,7 @@
 const chargebee = require("chargebee");
 chargebee.configure({
-  site: "driptextapp-test",  
-  api_key: "test_cd3amhfCG9p0aORToc1SOFysuhFs1Hiei"  
+  site: "driptextapp-test",
+  api_key: "test_eswb5pYvm5cuf87MYcdGyEcuTCiCJ5Uy6jk",
 });
 
 module.exports = chargebee;
